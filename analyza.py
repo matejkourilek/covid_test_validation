b6645e6b8cc1aada5057b5876e62d74049e083fa
Kouřílek Matěj <matej.kourilek@gasnet.cz>
2021-01-15 00:53:36 +0100

    data merging and plot with test result in title

--- analyza.py
+++ analyza.py
@@ -1,27 +1,46 @@
 import os
 import pandas as pd
 import numpy as np
-
-# Import the necessaries libraries
-import plotly.offline as pyo
-import plotly.graph_objs as go
-# Set notebook mode to work in offline
-pyo.init_notebook_mode()
-
-data = pd.read_excel('Data/00-ResultsFull_JK.xlsx', sheet_name='Data')
-results = pd.read_excel('Data/00-ResultsFull_JK.xlsx', sheet_name='Výsledky').dropna()
-data['id'] = data['TestBatchCode'] + '_' + data['SamplingItemCode'] + '_' + data['TestResultPosition']
+import matplotlib
+data = pd.read_excel('data/00-ResultsFull_JK.xlsx', sheet_name='Data')
+results = pd.read_excel('data/00-ResultsFull_JK.xlsx', sheet_name='Výsledky')
+data['id'] = data['TestBatchCode'] + '_' + data['SamplingItemCode'] #+ '_' + data['TestResultPosition']
 results['id'] = results['testbatchcode'] + '_' + results['samplingitemcode']
+results.set_index(['id'],drop=True, inplace=True)
+data.set_index(['id'],drop=False, inplace=True)
 
+# clean results with NaNs and inconclusive test
+results.result_value.unique()
+res = results[results['result_value'].notna()]
+res = res[~(res['result_value']=='INVALID')]
+res.shape
+res.samplestate.unique()
+res.drop(['samplestate'],axis=1, inplace=True)
 data.head()
-results.head()
+res.head()
+data.shape
+df_f = res.merge(data, how='outer', left_index=True, right_index=True)
+df_f.shape
 
-df = data.set_index(['id', 'Gen', 'Cycle'], drop=True)
-df = df.drop(columns=['TestBatchCode', 'TestResultPosition', 'SamplingItemCode', 'Rn'])
-
-df = df.unstack(level='Gen')
+df_f.dropna(subset=['TestResultPosition', 'SamplingItemCode'], axis=0, inplace=True)
+df_f.drop(['id'], axis=1, inplace=True)
+df_f.columns
+df_f['id'] = df_f['TestBatchCode'] + '_' + df_f['SamplingItemCode'] + '_' + df_f['TestResultPosition']
+df_f = df_f.set_index(['id', 'Gen', 'Cycle'], drop=True)
+df_f = df_f.drop(columns=['TestBatchCode', 'TestResultPosition', 'SamplingItemCode', 'Rn'])
+len(df_f)
+df_f = df_f[~df_f.index.duplicated(keep='first')]
+df = df_f.unstack(level='Gen')
 df = df.droplevel(level=0, axis=1)
 df = df.fillna(method='ffill')
 
-id = df.index.get_level_values(0).unique()[1]
-df.loc[id, :].plot()
+# sample 200 is negative while graph shows nice positivity. Same for sample number 500.
+# MIGHT HAVE ERROR IN MERGING
+id = df.index.get_level_values(0).unique()[200]
+df_f.loc[df_f.index.get_level_values(0)==id,'resultlevel'].unique()
+df_f.loc[df_f.index.get_level_values(0)==id,'result_value'][1]
+df.loc[id, :].plot(title=df_f.loc[df_f.index.get_level_values(0)==id,'result_value'][1])
+
+
+
+
